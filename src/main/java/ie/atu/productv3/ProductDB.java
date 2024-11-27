ef5ebf71a542f3d302d2af3be7a5b3004bf65f75
G00407095@atu.ie <G00407095@atu.ie>
2024-11-27 17:52:20 +0000

    task 2 completed; added two new product classes being tv and music. Now the application works with all products

--- src/main/java/ie/atu/productv3/ProductDB.java
+++ src/main/java/ie/atu/productv3/ProductDB.java
@@ -5,6 +5,8 @@ public class ProductDB {
     public static Product getProduct(String productCode) {
 
         Product myItem = null;
+
+        // BOOKS
         if (productCode.equalsIgnoreCase("java")) {
             Book myBook = new Book();
             myBook.setCode(productCode);
@@ -26,6 +28,8 @@ public class ProductDB {
             myBook.setPrice(54.50);
             myBook.setAuthor("Jim Lennon");
             myItem = myBook;
+
+            // SOFTWARE
         } else if (productCode.equalsIgnoreCase("studios")) {
             Software mySoftware = new Software();
             mySoftware.setCode(productCode);
@@ -47,8 +51,45 @@ public class ProductDB {
             mySoftware.setPrice(54.50);
             mySoftware.setVersion("Oracle 3.0");
             myItem = mySoftware;
+
+            // MUSIC
+        } else if (productCode.equalsIgnoreCase("pink")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Wish You Were Here");
+            myMusic.setPrice(9.99);
+            myMusic.setArtist("Pink Floyd");
+            myMusic.setLabel("Columbia Group");
+            myItem = myMusic;
+        } else if (productCode.equalsIgnoreCase("Mike")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Pieces Of A Dream");
+            myMusic.setPrice(99.99);
+            myMusic.setArtist("Mike");
+            myMusic.setLabel("New York Records");
+            myItem = myMusic;
+
+            // TV's
+        } else if (productCode.equalsIgnoreCase("Samsung")) {
+            Tv tv = new Tv();
+            tv.setCode(productCode);
+            tv.setDescription("4K 60hz");
+            tv.setPrice(999.99);
+            tv.setManufacturer("Samsung");
+            tv.setScreenSize("50 Inch");
+            myItem = tv;
+        } else if (productCode.equalsIgnoreCase("Asus")) {
+            Tv tv = new Tv();
+            tv.setCode(productCode);
+            tv.setDescription("1440p 120hz");
+            tv.setPrice(199.99);
+            tv.setManufacturer("Philips");
+            tv.setScreenSize("24 Inch");
+            myItem = tv;
         }
         return myItem;
     }
 }
+
 
